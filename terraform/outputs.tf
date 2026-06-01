367e1299f71ae359134c49dd085af9471462d4a2
Fatmaosama25 <fattmmaa@users.noreply.github.com>
2026-06-02 00:53:24 +0300

    Increase dataset to 200 files - 72.7% cost reduction,  annual savings

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_arn" {
   value = aws_s3_bucket.optimized_storage.arn
 }
-output "monthly_savings" { value = "$4406.67" }
-output "annual_savings" { value = "$52880.04" }
-output "cost_reduction" { value = "59.2%" }
-output "files_migrated" { value = 14 }
+output "monthly_savings" { value = "$52249.66" }
+output "annual_savings" { value = "$626995.92" }
+output "cost_reduction" { value = "72.7%" }
+output "files_migrated" { value = 57 }
